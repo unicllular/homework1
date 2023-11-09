5ae4fafd363541cb262f52cb591879c270b27599
unicllular <150330036+unicllular@users.noreply.github.com>
2023-11-09 12:28:52 +0330

    Add files via upload

---
+++ practice4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("M                  M\n");
+    printf("M M              M M\n");
+    printf("M  M            M  M\n");
+    printf("M   M          M   M\n");
+    printf("M    M        M    M\n");
+    printf("M     M      M     M\n");
+    printf("M      M    M      M\n");
+    printf("M       M  M       M\n");
+    printf("M        M         M");
+
+return 0;
+}
+
---
+++ practice5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("oOoOo\n");
+    printf("oOoOo\n");
+    printf("oOoOo\n");
+return 0;
+}
+
